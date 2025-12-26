bac40f4ddee95f4728c31291a28372f59aff41d0
Preran <Preran@Avanash.RCPL.LOCAL>
2025-12-26 15:33:04 +0200

    Empty repository - remove all files

--- frontend/src/components/Footer.vue
+++
@@ -1,5 +0,0 @@
-<template>
-  <footer class="text-tertiary/70 text-sm">
-    &copy; 2025 RC Convergio. All rights reserved.
-  </footer>
-</template>
--- frontend/src/views/documents/DocumentsPage.vue
+++
@@ -1,9 +0,0 @@
-<template>
-  <div class="documents-page">
-    <DocumentsLibrary />
-  </div>
-</template>
-
-<script setup>
-import DocumentsLibrary from '@/components/documents/DocumentsLibrary.vue'
-</script>
